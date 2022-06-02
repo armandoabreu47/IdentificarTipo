b861dc6148f76b2e6ef83713855e537fc9b1d8c8
Armando Abreu <38231368+armandoabreu47@users.noreply.github.com>
2022-06-02 11:23:16 -0300

    Add files via upload

---
+++ EX004.py
@@ -0,0 +1,8 @@
+a = input('Digite algo: ')
+print('O tipo primitivo desse valor é :', type(a))
+print('Só tem espaços?', a.isspace())
+print('É um número?', a.isnumeric())
+print('É alfabético? ', a.isalpha())
+print('Maiúscula?', a.isupper())
+print('Minúscula?', a.islower())
+
